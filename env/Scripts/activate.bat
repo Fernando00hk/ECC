891334f51c1fe0eb5a5f59ff538665850bbff686
Fernando00hk <jorgefernando31609@gmail.com>
2024-03-10 23:38:38 -0600

    segundo commit

--- env/Scripts/activate.bat
+++ env/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=D:\proyecto\env
+set VIRTUAL_ENV=C:\Users\Administrator\Desktop\ECC\env
 
 if not defined PROMPT set PROMPT=$P$G
 
